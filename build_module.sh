5317fb4e2988aed953d89793e2c92c8e3745a32a
hejin <hejinpy@163.com>
2020-03-09 09:45:08 +0800

    convert ctrl to lf, add build_module.sh

---
+++ build_module.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+python3 process_init.py
+python3 process_global_variables.py
+python3 process_strings.py
+python3 process_skills.py
+python3 process_music.py
+python3 process_animations.py
+python3 process_meshes.py
+python3 process_sounds.py
+python3 process_skins.py
+python3 process_map_icons.py
+python3 process_factions.py
+python3 process_items.py
+python3 process_scenes.py
+python3 process_troops.py
+python3 process_particle_sys.py
+python3 process_scene_props.py
+python3 process_tableau_materials.py
+python3 process_presentations.py
+python3 process_party_tmps.py
+python3 process_parties.py
+python3 process_quests.py
+python3 process_info_pages.py
+python3 process_scripts.py
+python3 process_mission_tmps.py
+python3 process_game_menus.py
+python3 process_simple_triggers.py
+python3 process_dialogs.py
+python3 process_global_variables_unused.py
+python3 process_postfx.py
+
+rm *pyc
+
+echo '______________________________'
+echo 'Script processing has ended.'
--- module_info_pages.py
+++ module_info_pages.py
@@ -1,4 +1,4 @@
-﻿####################################################################################################################
+####################################################################################################################
 #  Each quest record contains the following fields:
 #  1) Info page id: used for referencing info pages in other files. The prefix ip_ is automatically added before each info page id.
 #  2) Info page name: Name displayed in the info page screen.
