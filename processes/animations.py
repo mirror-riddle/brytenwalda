e1022de4b5f935e689a3d6bafb28f9f2b9b7f479
mirror-riddle <hejinpy@163.com>
2020-03-10 20:23:46 +0800

    fix process animations

--- processes/animations.py
+++ processes/animations.py
@@ -1,64 +1,48 @@
 from modules.info import export_dir
 from modules.animations import animations
-from processes.common import lf_open
+from common import lf_open
 
 
-def compile_action_sets(actions):
-    action_codes = []
-    for action in actions:
-        index = -1
-        for i_action_code in range(len(action_codes)):
-            if action_codes[i_action_code] == action[0]:
-                index = i_action_code
-                break
-        if index == -1:
-            pos = len(action_codes)
-            action_codes.append(action[0])
-            action[0] = pos
+def init_actions_file():
+    file = open(export_dir + "actions.txt", "w")
+    file.write("%d\n" % len(animations))
+    return file
+
+
+def write_actions_file(file, action):
+    file.write(" %s %d %d " % (action[0], action[1], action[2]))
+    file.write(" %d\n" % (len(action) - 3))
+    for elem in action[3:]:
+        file.write("  %f %s %d %d %d " % (elem[0], elem[1], elem[2], elem[3], elem[4]))
+        if (len(elem) > 5):
+            file.write("%d " % elem[5])
         else:
-            action[0] = index
-    return action_codes
+            file.write("0 ")
+        if (len(elem) > 6):
+            file.write("%f %f %f  " % elem[6])
+        else:
+            file.write("0.0 0.0 0.0 ")
+        if (len(elem) > 7):
+            file.write("%f \n" % (elem[7]))
+        else:
+            file.write("0.0 \n")
 
 
-def write_actions(action_set, num_action_codes, action_codes, file_name):
-    file = open(export_dir + file_name, "w")
-    file.write("%d\n" % num_action_codes)
-    for i_action_code in range(num_action_codes):
-        action_found = 0
-        for action in action_set:
-            if action[0] == i_action_code:
-                file.write(" %s %d %d " % (action_codes[i_action_code], action[1], action[2]))  # print flags
-                file.write(" %d\n" % (len(action)-3))
-                for elem in action[3:]:
-                    file.write("  %f %s %d %d %d " % (elem[0], elem[1], elem[2], elem[3], elem[4]))
-                    if (len(elem) > 5):
-                        file.write("%d " % elem[5])
-                    else:
-                        file.write("0 ")
-                    if (len(elem) > 6):
-                        file.write("%f %f %f  " % elem[6])
-                    else:
-                        file.write("0.0 0.0 0.0 ")
-                    if (len(elem) > 7):
-                        file.write("%f \n" % (elem[7]))
-                    else:
-                        file.write("0.0 \n")
-                action_found = 1
-                break
-        if not action_found:
-            file.write(" none 0 0\n")  # oops
+def write_ids_file(file, name, index):
+    file.write("anim_%s = %d\n" % (name, index))
 
 
-def save_python_header(action_codes):
-    ofile = lf_open("../ids/animations.py", "w")
-    for i_anim in range(len(action_codes)):
-        ofile.write("anim_%s = %d\n" % (action_codes[i_anim], i_anim))
-    ofile.write("\n\n")
-    ofile.close()
+def close_ids_file(file):
+    file.write("\n\n")
+    file.close()
 
 
 def process_animations():
     print("Exporting animations...")
-    action_codes = compile_action_sets(animations)
-    save_python_header(action_codes)
-    write_actions(animations, len(action_codes), action_codes, "actions.txt")
+    actions_file = init_actions_file()
+    ids_file = lf_open("../ids/animations.py", "w")
+    for (index, animation) in enumerate(animations):
+        write_ids_file(ids_file, animation[0], index)
+        write_actions_file(actions_file, animation)
+    actions_file.close()
+    close_ids_file(ids_file)
