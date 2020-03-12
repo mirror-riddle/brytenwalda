9d579df7427daa0543eb8a4d2c2922fd1f01d21d
hejin <hejinpy@163.com>
2020-03-12 10:02:15 +0800

    update process info_pages

--- processes/info_pages.py
+++ processes/info_pages.py
@@ -1,28 +1,40 @@
 from modules.info import export_dir
 from modules.info_pages import info_pages
-from common import lf_open
+from common import lf_open, replace_spaces
+from io_processor import IOProcessor
 
 
-def save_info_pages():
-    ofile = open(export_dir + "info_pages.txt", "w", -1, "utf-8")
-    ofile.write("infopagesfile version 1\n")
-    ofile.write("%d\n" % (len(info_pages)))
-    for i_info_page in range(len(info_pages)):
-        info_page = info_pages[i_info_page]
-        ofile.write("ip_%s %s %s" % (info_page[0], info_page[1].replace(" ", "_"), info_page[2].replace(" ", "_")))
-        ofile.write("\n")
-    ofile.close()
+class IOIDs(IOProcessor):
+
+    def write(self, name, index):
+        self.file.write("ip_%s = %d\n" % (name, index))
+
+    def before_close(self):
+        self.file.write("\n\n")
 
 
-def save_python_header():
-    ofile = lf_open("../ids/info_pages.py", "w")
-    for i_info_page in range(len(info_pages)):
-        ofile.write("ip_%s = %d\n" % (info_pages[i_info_page][0], i_info_page))
-    ofile.write("\n\n")
-    ofile.close()
+class IOInfoPages(IOProcessor):
+
+    def after_open(self):
+        self.file.write("infopagesfile version 1\n")
+        self.file.write("%d\n" % len(info_pages))
+
+    def write(self, info_page):
+        title = replace_spaces(info_page[1])
+        content = replace_spaces(info_page[2])
+        self.file.write("ip_%s %s %s" % (info_page[0], title, content))
+        self.file.write("\n")
 
 
 def process_info_pages():
-    print("Exporting info_page data...")
-    save_info_pages()
-    save_python_header()
+    print("Exporting info_page...")
+
+    io_ids = IOIDs("../ids/info_pages.py")
+    io_info_pages = IOInfoPages(export_dir + "info_pages.txt")
+
+    for (index, info_page) in enumerate(info_pages):
+        io_info_pages.write(info_page)
+        io_ids.write(info_page[0], index)
+
+    io_ids.close()
+    io_info_pages.close()
