d3fb4e75432db5adbedd27a61edb1cc6de5d43df
mirror-riddle <hejinpy@163.com>
2020-03-13 18:45:45 +0800

    update process tableau_materials

--- processes/tableau_materials.py
+++ processes/tableau_materials.py
@@ -1,45 +1,55 @@
 from modules.info import export_dir
 from modules.tableau_materials import tableaus
 from common import lf_open
-from operations import *
+from operations import (
+    save_statement_block, load_variables, load_quick_strings,
+    save_variables, save_quick_strings
+)
+from module_processor import ModuleProcessor
 
 
-def save_tableau_materials(variable_list, variable_uses, tag_uses, quick_strings):
-    ofile = open(export_dir + "tableau_materials.txt", "w")
-    ofile.write("%d\n" % (len(tableaus)))
-    for tableau in tableaus:
-        tableau_info = (
-            tableau[0],
-            tableau[1],
-            tableau[2],
-            tableau[3],
-            tableau[4],
-            tableau[5],
-            tableau[6],
-            tableau[7],
-            tableau[8]
-        )
-        ofile.write("tab_%s %d %s %d %d %d %d %d %d" % tableau_info)
-        save_statement_block(ofile, 0, 1, tableau[9], variable_list, variable_uses, tag_uses, quick_strings)
-        ofile.write("\n")
-    ofile.close()
+def save_tableau_materials(ofile, tableau):
+  tableau_info = (
+      tableau[0],
+      tableau[1],
+      tableau[2],
+      tableau[3],
+      tableau[4],
+      tableau[5],
+      tableau[6],
+      tableau[7],
+      tableau[8]
+  )
+  ofile.write("tab_%s %d %s %d %d %d %d %d %d" % tableau_info)
 
 
-def save_python_header():
-    ofile = lf_open("../ids/tableau_materials.py", "w")
-    for i_tableau in range(len(tableaus)):
-        ofile.write("tableau_%s = %d\n" % (tableaus[i_tableau][0], i_tableau))
-    ofile.close()
+class TableauMaterialProcessor(ModuleProcessor):
+  id_prefix = "tableau_"
+  id_name = "tableau_materials.py"
+  export_name = "tableau_materials.txt"
+
+  def after_open_export_file(self):
+    self.export_file.write("%d\n" % len(tableaus))
+
+  def write_export_file(self, tableau):
+    save_tableau_materials(self.export_file, tableau)
+
+  def save_statements(self, tableau, variables, variable_uses, quick_strings):
+    save_statement_block(self.export_file, 0, 1, tableau[9], variables, variable_uses, [], quick_strings)
+    self.export_file.write("\n")
 
 
 def process_tableau_materials():
-    print("Exporting tableau materials data...")
-    save_python_header()
-    variable_uses = []
-    variables = load_variables(export_dir, variable_uses)
-    tag_uses = load_tag_uses(export_dir)
-    quick_strings = load_quick_strings(export_dir)
-    save_tableau_materials(variables, variable_uses, tag_uses, quick_strings)
-    save_variables(export_dir, variables, variable_uses)
-    save_tag_uses(export_dir, tag_uses)
-    save_quick_strings(export_dir, quick_strings)
+  print("Exporting tableau materials data...")
+  variable_uses = []
+  variables = load_variables(export_dir, variable_uses)
+  quick_strings = load_quick_strings(export_dir)
+
+  processor = TableauMaterialProcessor()
+  for index, tableau in enumerate(tableaus):
+    processor.write(tableau, index)
+    processor.save_statements(tableau, variables, variable_uses, quick_strings)
+  processor.close()
+
+  save_variables(export_dir, variables, variable_uses)
+  save_quick_strings(export_dir, quick_strings)
