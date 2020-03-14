b2601078548f3f95fd9b9e367ee33efff4f2d505
mirror-riddle <hejinpy@163.com>
2020-03-14 15:18:13 +0800

    process party_templates

--- processes/party_tmps.py
+++ processes/party_tmps.py
@@ -1,50 +1,53 @@
 from modules.info import export_dir
 from modules.party_templates import party_templates
 from common import convert_to_identifier, replace_spaces, lf_open
+from module_processor import ModuleProcessor
 
 
 def save_party_template_troop(file, troop):
-    if troop:
-        #    add_tag_use(tag_uses,tag_troop,troop[0])
-        file.write("%d %d %d " % (troop[0], troop[1], troop[2]))
-        if (len(troop) > 3):
-            file.write("%d " % troop[3])
-        else:
-            file.write("0 ")
+  if troop:
+    file.write("%d %d %d " % (troop[0], troop[1], troop[2]))
+    if (len(troop) > 3):
+      file.write("%d " % troop[3])
     else:
-        file.write("-1 ")
+      file.write("0 ")
+  else:
+    file.write("-1 ")
 
 
-def save_party_templates():
-    file = open(export_dir + "party_templates.txt", "w")
-    file.write("partytemplatesfile version 1\n")
-    file.write("%d\n" % (len(party_templates)))
-    for party_template in party_templates:
-        #    add_tag_use(tag_uses,tag_faction,party_template[4])
-        file.write("pt_%s %s %d %d %d %d " % (convert_to_identifier(party_template[0]), replace_spaces(
-            party_template[1]), party_template[2], party_template[3], party_template[4], party_template[5]))
-        members = party_template[6]
-        if (len(members) > 6):
-            print("Error! NUMBER OF TEMPLATE MEMBERS EXCEEDS 6 " + party_template[0])
-            members = members[0:6]
-        for party_template_member in members:
-            save_party_template_troop(file, party_template_member)
-        for i in range(6 - len(members)):
-            save_party_template_troop(file, 0)
-        file.write("\n")
-    file.close()
+def save_party_template(file, template):
+  identity = convert_to_identifier(template[0])
+  name = replace_spaces(template[1])
+  file.write("pt_%s %s %d %d %d %d " %(
+    identity, name, template[2], template[3], template[4], template[5]
+  ))
+  members = template[6]
+  if (len(members) > 6):
+    print("Error! NUMBER OF TEMPLATE MEMBERS EXCEEDS 6 " + template[0])
+    members = members[0:6]
+  for member in members:
+    save_party_template_troop(file, member)
+  for i in range(6 - len(members)):
+    save_party_template_troop(file, 0)
+  file.write("\n")
 
 
-def save_python_header():
-    file = lf_open("../ids/party_templates.py", "w")
-    for i_party_template in range(len(party_templates)):
-        file.write("pt_%s = %d\n" % (convert_to_identifier(party_templates[i_party_template][0]), i_party_template))
-    file.close()
+class PartyTemplateProcessor(ModuleProcessor):
+  id_prefix = "pt_"
+  id_name = "party_templates.py"
+  export_name = "party_templates.txt"
+
+  def after_open_export_file(self):
+    self.export_file.write("partytemplatesfile version 1\n")
+    self.export_file.write("%d\n" % len(party_templates))
+
+  def write_export_file(self, patry_template):
+    save_party_template(self.export_file, patry_template)
 
 
 def process_party_tmps():
-    print("Exporting party_template data...")
-    #tag_uses = load_tag_uses(export_dir)
-    save_python_header()
-    save_party_templates()
-    #save_tag_uses(export_dir, tag_uses)
+  print("Exporting party_templates...")
+  processor = PartyTemplateProcessor()
+  for index, party_template in enumerate(party_templates):
+    processor.write(party_template, index)
+  processor.close()
